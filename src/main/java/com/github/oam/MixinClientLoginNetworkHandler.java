f7db6a3011ac1d699c6ce738c569a6b6018c80d2
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2021-10-07 21:33:10 +0200

    Initial commit

---
+++ src/main/java/com/github/oam/MixinClientLoginNetworkHandler.java
@@ -0,0 +1,63 @@
+package com.github.oam;
+
+import io.netty.buffer.Unpooled;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ConfirmScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.network.ClientLoginNetworkHandler;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.packet.c2s.login.LoginQueryResponseC2SPacket;
+import net.minecraft.network.packet.s2c.login.LoginQueryRequestS2CPacket;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientLoginNetworkHandler.class)
+public abstract class MixinClientLoginNetworkHandler {
+
+    @Shadow
+    @Final
+    private MinecraftClient client;
+
+    @Shadow
+    @Final
+    private ClientConnection connection;
+
+    @Shadow
+    @Nullable
+    protected abstract Text joinServerSession(String serverId);
+
+    @Inject(method = "onQueryRequest", at = @At(value = "INVOKE", target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V", remap = false), cancellable = true)
+    public void handleOAM(LoginQueryRequestS2CPacket packet, CallbackInfo ci) {
+        if (packet.channel.toString().equals("openauthmod:join")) {
+            ci.cancel();
+
+            final Screen parentScreen = this.client.currentScreen;
+            final String serverHash = packet.payload.readString();
+            this.client.execute(() -> {
+                this.client.openScreen(new ConfirmScreen(success -> {
+                    if (success) {
+                        if (this.joinServerSession(serverHash) == null) {
+                            this.client.openScreen(parentScreen);
+                            this.connection.send(new LoginQueryResponseC2SPacket(packet.queryId, new PacketByteBuf(Unpooled.buffer())));
+                        } else {
+                            this.client.openScreen(parentScreen);
+                            this.connection.send(new LoginQueryResponseC2SPacket(packet.queryId, null));
+                        }
+                    } else {
+                        this.client.openScreen(parentScreen);
+                        this.connection.send(new LoginQueryResponseC2SPacket(packet.queryId, null));
+                    }
+                }, new LiteralText("Allow Open Auth Mod authentication?"), new LiteralText("This will allow the proxy to authenticate as you in a Minecraft Server.")));
+            });
+        }
+    }
+
+}
