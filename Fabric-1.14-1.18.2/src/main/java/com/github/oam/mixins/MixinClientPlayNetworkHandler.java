d793fd8dd94abc6a0facf5e532a573a87f6c73a0
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2022-02-22 18:56:19 +0100

    Improved code

--- Fabric-1.14-1.18.2/src/main/java/com/github/oam/mixins/MixinClientPlayNetworkHandler.java
+++ Fabric-1.14-1.18.2/src/main/java/com/github/oam/mixins/MixinClientPlayNetworkHandler.java
@@ -2,8 +2,11 @@ package com.github.oam.mixins;
 
 import com.github.oam.OpenAuthMod;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.ClientConnection;
 import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,9 +16,13 @@ import java.io.IOException;
 @Mixin(value = ClientPlayNetworkHandler.class)
 public abstract class MixinClientPlayNetworkHandler {
 
+    @Shadow
+    @Final
+    private ClientConnection connection;
+
     @Inject(method = "onCustomPayload", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetworkThreadUtils;forceMainThread(Lnet/minecraft/network/Packet;Lnet/minecraft/network/listener/PacketListener;Lnet/minecraft/util/thread/ThreadExecutor;)V"), cancellable = true)
     private void handleOAM(CustomPayloadS2CPacket packet, CallbackInfo ci) throws IOException {
-        if (OpenAuthMod.INSTANCE.handlePlayCustomPayload(((ClientPlayNetworkHandler) (Object) this).getConnection(), packet.channel, packet.data)) {
+        if (OpenAuthMod.INSTANCE.handlePlayCustomPayload(this.connection, packet.channel, packet.data)) {
             ci.cancel();
         }
     }
